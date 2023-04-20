58a7917c20a2d5f82d2d2d875e030a059c6461d0
F4ELL <ferreiraperes1234@gmail.com>
2023-04-19 21:04:37 -0300

    feat: banner home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,54 @@
-import { HomeContainer } from "./styles";
+import { BannerIcon, BannerIntro, BannerIntroContent, BannerQualities, BannerQualitiesItem, BannerTextArea, HomeContainer } from "./styles";
+import CoffeeImg from '../../assets/coffee.png'
+import { ShoppingCart, Package, Coffee, Watch } from 'phosphor-react'
 
 export function Home() {
     return (
         <HomeContainer>
-            <h1>Home</h1>
+            <BannerIntro>
+                <BannerIntroContent>
+                    <BannerTextArea>
+                        <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+                        <p>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</p>
+
+                        <BannerQualities>
+                            <BannerQualitiesItem>
+                                <BannerIcon
+                                    variant="shop"
+                                >
+                                    <ShoppingCart size={16} weight="fill" color="white"/>
+                                </BannerIcon>
+                                <span>Compra simples e segura</span>
+                            </BannerQualitiesItem>
+                            <BannerQualitiesItem>
+                                <BannerIcon
+                                    variant="package"
+                                >
+                                    <Package size={16} weight="fill" color="white"/>
+                                </BannerIcon>
+                                <span>Embalagem mantém o café intacto</span>
+                            </BannerQualitiesItem>
+                            <BannerQualitiesItem>
+                                <BannerIcon
+                                    variant="watch"
+                                >
+                                    <Watch size={16} weight="fill" color="white"/>
+                                </BannerIcon>
+                                <span>Entrega rápida e rastreada</span>
+                            </BannerQualitiesItem>
+                            <BannerQualitiesItem>
+                                <BannerIcon
+                                    variant="coffee"
+                                >
+                                    <Coffee size={16} weight="fill" color="white"/>
+                                </BannerIcon>
+                                <span>O café chega fresquinho até você</span>
+                            </BannerQualitiesItem>
+                        </BannerQualities>
+                    </BannerTextArea>
+                    <img src={CoffeeImg} alt="" />
+                </BannerIntroContent>
+            </BannerIntro>
         </HomeContainer>
     )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,5 +1,88 @@
 import styled from 'styled-components'
+import EffectBg from '../../assets/bg-intro.png'
 
-export const HomeContainer = styled.div`
+export const HomeContainer = styled.main`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+`
+
+export const BannerIntro = styled.section`
+    background: ${() => `url(${EffectBg}) no-repeat center`};
+    background-size: cover;
+`
+
+export const BannerIntroContent = styled.div`
+    width: 100%;
+    max-width: 70rem;
+    margin: 0 auto;
     
+    padding: 5.875rem 0;
+
+    display: flex;
+    gap: 3.5rem;
+`
+
+export const BannerTextArea = styled.div`
+    width: 36.75rem;
+
+    display: flex;
+    flex-direction: column;
+
+    h1 {
+        font-family: 'Baloo 2', cursive;
+        font-size: 3rem;
+        font-weight: 800;
+        color: ${({ theme }) => theme['base-title']};
+        line-height: 62.4px;
+    }
+
+    p {
+        font-size: 1.25rem;
+        color: ${({ theme }) => theme['base-subtitle']};
+        line-height: 26px;
+        margin-top: 1rem;
+    }
+`
+
+export const BannerQualities = styled.div`
+    display: grid;
+    grid-template-columns: 16rem 1fr;
+    gap: 1.25rem;
+
+    margin-top: 4.125rem;
+`
+
+export const BannerQualitiesItem = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 0.875rem;
+
+    span {
+        font-size: 1rem;
+        line-height: 20.8px;
+    }
+`
+
+export type Props = {
+    variant: "shop" | "package" | "watch" | "coffee" 
+}
+
+const colorsIcons = {
+    shop: "yellow-dark",
+    package: "base-text",
+    watch: "yellow",
+    coffee: "purple"
+} as const
+
+export const BannerIcon = styled.div<Props>`
+    width: 2rem;
+    height: 2rem;
+    border-radius: 50%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    background-color: ${({ theme, variant }) => theme[colorsIcons[variant]]};
 `
